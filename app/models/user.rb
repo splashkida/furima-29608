12a674bc735372a0bd8344076a863591e54eb610
splashkida <seira.kida0406@gmail.com>
2020-12-10 21:52:34 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :encrypted_password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+          validates :last_name, :first_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+          validates :last_name_kana, :first_name_kana,format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを使用してください' }
+          validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+          validates :birthday
+
+         end
 end
