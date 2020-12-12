d4760bae1d7a81301e99f2785e8b9312c6f3cad6
splashkida <seira.kida0406@gmail.com>
2020-12-12 16:47:12 +0900

    ログイン・ログアウト機能実装２

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
+  before_action :move_to_index, except: [:index]
   
-def index
-end
+  def index
+  end
 
 
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
 end
