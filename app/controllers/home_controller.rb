d38f0e8cc2e3b5ee81668309dd418b327362e391
splashkida <seira.kida0406@gmail.com>
2020-12-14 00:49:34 +0900

    Rubocopを実行

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,12 @@
 class HomeController < ApplicationController
   before_action :move_to_index, except: [:index]
-  
+
   def index
   end
-
-
 
   private
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :items
-         has_many :purchase_records
+  has_many :items
+  has_many :purchase_records
 
-         
-         with_options presence: true do
-          validates :nickname
-          validates :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-          validates :last_name, :first_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
-          validates :last_name_kana, :first_name_kana,format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを使用してください' }
-          validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
-          validates :birthday
-         end
-
-          
+  with_options presence: true do
+    validates :nickname
+    validates :password, :password_confirmation, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カタカナを使用してください' }
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+    validates :birthday
+  end
 end
