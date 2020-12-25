fdf3fe7ddc5252e1187e2178df59f3270879e58b
splashkida <seira.kida0406@gmail.com>
2020-12-25 21:43:37 +0900

    修正
    ログインしていないユーザーでも商品の詳細を見ることができる。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
   end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def new
     @item = Item.new
